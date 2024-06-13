5d169d0f5403f41e7438f80be09b1143df99884a
Kristof De Moor <kristof.demoor@protonmail.com>
2024-06-13 18:55:01 +0200

    game over buttons

--- Lab/src/scenes/gameOver.js
+++ Lab/src/scenes/gameOver.js
@@ -4,23 +4,75 @@ export default class Game extends Phaser.Scene {
     }
 
     preload() {
-        this.load.audio("gameover", "./assets/sounds/gameover.mp3");
+        // Image
+        this.load.image("background", "./assets/gameover.png");
+
+        // Sounds
+        this.load.audio("gameOverVoice", "./assets/sounds/gameover.mp3");
     }
 
     create() {
-        this.add.text(100, 200, "GAME OVER", {
-            fontFamily: "DS-Digital",
-            fontSize: 100,
-            color: "#a0a0a0",
-        });
+        // Background
+        this.add.image(300, 400, "background");
 
+        // Buttons
+        this.createRetryButton();
+        this.createQuitButton();
+
+        // Sound
         this.time.delayedCall(
             1000,
             () => {
-                this.sound.add("gameover").play();
+                this.sound.add("gameOverVoice").play();
             },
             null,
             this
         );
+    }
+
+    createRetryButton() {
+        const retryButton = this.add.text(270, 600, "retry", {
+            fontFamily: "BAD GRUNGE",
+            fontSize: 56,
+            color: "#ffffff",
+        });
+        retryButton.setInteractive();
+
+        retryButton.on("pointerover", () => {
+            retryButton.setX(261);
+            retryButton.setY(590);
+            retryButton.setFontSize(70);
+        });
+
+        retryButton.on("pointerout", () => {
+            retryButton.setX(270);
+            retryButton.setY(600);
+            retryButton.setFontSize(56);
+        });
+
+        retryButton.on("pointerdown", () => {
+            this.scene?.start("gameScene");
+        });
+    }
+
+    createQuitButton() {
+        const quitButton = this.add.text(285, 660, "quit", {
+            fontFamily: "BAD GRUNGE",
+            fontSize: 56,
+            color: "#ffffff",
+        });
+        quitButton.setInteractive();
+
+        quitButton.on("pointerover", () => {
+            quitButton.setX(278);
+            quitButton.setY(650);
+            quitButton.setFontSize(70);
+        });
+
+        quitButton.on("pointerout", () => {
+            quitButton.setX(285);
+            quitButton.setY(660);
+            quitButton.setFontSize(56);
+        });
     }
 }
